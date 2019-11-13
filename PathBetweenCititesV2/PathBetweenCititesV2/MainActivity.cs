9d88e994384d7f83f6b16371685354b8ffad1fb5
Samuel Dias <42343502+Nikenilson@users.noreply.github.com>
2019-11-13 11:05:08 -0300

    c

--- PathBetweenCititesV2/PathBetweenCititesV2/MainActivity.cs
+++ PathBetweenCititesV2/PathBetweenCititesV2/MainActivity.cs
@@ -1,19 +1,30 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
 namespace PathBetweenCititesV2
 {
     [Activity(Label = "PathBetweenCititesV2", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        Button btnAddCidade;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            btnAddCidade = FindViewById<Button>(Resource.Id.btnAddCidade);
+            btnAddCidade.Click += delegate
+            {
+                var intent = new Intent(this, typeof(AdicionarActivity));
+                StartActivity(intent);
+            };
+            
         }
+
     }
 }
 
--- PathBetweenCititesV2/PathBetweenCititesV2/obj/Debug/android/pathbetweencititesv2/pathbetweencititesv2/R.java
+++ PathBetweenCititesV2/PathBetweenCititesV2/obj/Debug/android/pathbetweencititesv2/pathbetweencititesv2/R.java
@@ -23,7 +23,8 @@ public final class R {
         public static final int textView2=0x7f040002;
     }
     public static final class layout {
-        public static final int main=0x7f020000;
+        public static final int adicionar=0x7f020000;
+        public static final int main=0x7f020001;
     }
     public static final class string {
         public static final int app_name=0x7f030000;
